6a779d65c13e80c5b902dae15f2193fb2d175cb4
vhernandesjr <valmirhjr@live.com>
2017-06-02 10:53:11 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,9 +1,9 @@
 <?php
 
 	$db_host = "mysql.dgplus.com.br"; 
-    $db_user = "dgplus02"; 
-    $db_pass = "agenda2016"; 
-    $db_name   = "dgplus02";
+    $db_user = "agenda2016"; 
+    $db_pass = "alundra2016"; 
+    $db_name   = "agenda2016";
 
 $conexao = mysql_connect($db_host, $db_user, $db_pass) or die("Não foi possível conectar à base de dados. Erro: " . mysql_errno() . "<br />" . mysql_error());
 mysql_select_db($db_name, $conexao) or die("Não foi possível selecionar o banco de dados. Erro: " . mysql_errno() . "<br />" . mysql_error());
